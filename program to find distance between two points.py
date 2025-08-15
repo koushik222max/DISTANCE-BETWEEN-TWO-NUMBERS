76da247319118c1fa59322e3cd5aa329c49f3ce4
Koushik Raj <koushikraj768@gmail.com>
2025-08-15 21:31:52 +0530

    Add files via upload

---
+++ program to find distance between two points.py
@@ -0,0 +1,9 @@
+print('If point value is none enter \'0\' ')
+a=int(input('\nEnter value of x1:'))
+b=int(input('Enter value of x2:'))
+c=int(input('Enter value of y1:'))
+d=int(input('Enter value of y2:'))
+
+e=       ((((b-a)**2)+((d-c)**2))**0.5)
+
+print('\nThe distance between two points is:',e)
